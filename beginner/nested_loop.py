e4c89b6beec4d2ef3183547622fb230b106e61ce
Roman <roman@Romans-MacBook-Air.local>
2026-05-13 22:03:29 +0545

    started learning about nested loop deeply

---
+++ beginner/nested_loop.py
@@ -0,0 +1,13 @@
+# syntax
+# outer loop
+#     inner loop
+#         statement(s) for inner loop
+# block of code for outer loop
+
+# for i in range(3):      # outer loop
+#     for j in range(2):  # inner loop
+#         print(i, j)
+# for i in range(5):    
+#     for j in range(5):
+#         print("*", end=" ")
+#     print()
